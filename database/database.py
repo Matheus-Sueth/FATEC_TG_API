f0f1c0d1bc1891b03f618f4772dc8830881e2dbe
Matheus <matheuzengenharia@gmail.com>
2022-10-23 16:42:18 -0300

    Mudando para PostgreSQL parte 2

--- database/database.py
+++ database/database.py
@@ -3,13 +3,11 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 #SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:matheus123@localhost/sql_app"
+SQLALCHEMY_DATABASE_URL = "postgres://lrnyytepiqbrtt:eb4a684dd19c817051049c495f7dc1a6e99b2188d7bc46547c111b993a918f68@ec2-44-210-228-110.compute-1.amazonaws.com:5432/d1dm3sv1kmqaf2"
 
-engine = create_engine(
-   SQLALCHEMY_DATABASE_URL)#, connect_args={"check_same_thread": False}
-#)
+#engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
 
-# engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
