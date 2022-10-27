bd67bbcffb1c6c5c271a33047eefc6ff6cd76a97
Matheus <matheuzengenharia@gmail.com>
2022-10-27 00:25:27 -0300

    fix: Alterando o banco de dados parte 2

--- database/database.py
+++ database/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 #SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
-SQLALCHEMY_DATABASE_URL = "postgresql://lcrqrxainnruur : a06458298899475eb468a56bd8637706b9c53534f2809e90375be375982fc134 @ ec2-44-199-9-102.compute-1.amazonaws.com mg : 5432 / d4vn6ip4sj7n6ip4sj7"
+SQLALCHEMY_DATABASE_URL = "postgresql://lcrqrxainnruur:a06458298899475eb468a56bd8637706b9c53534f2809e90375be375982fc134@ec2-44-199-9-102.compute-1.amazonaws.com:5432/d4vn6ip4sj7n6ip4sj7"
 
 #engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
 
