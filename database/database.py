0125ce63075cb1714ac1e9fdc3eb999708aae6e3
Matheus <matheuzengenharia@gmail.com>
2022-10-27 00:15:14 -0300

    fix: Alterando o banco de dados

--- database/database.py
+++ database/database.py
@@ -2,12 +2,12 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
-#SQLALCHEMY_DATABASE_URL = "postgresql+psycopg2://lrnyytepiqbrtt:eb4a684dd19c817051049c495f7dc1a6e99b2188d7bc46547c111b993a918f68@ec2-44-210-228-110.compute-1.amazonaws.com:5432/d1dm3sv1kmqaf2"
+#SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
+SQLALCHEMY_DATABASE_URL = "postgresql://lrnyytepiqbrtt:eb4a684dd19c817051049c495f7dc1a6e99b2188d7bc46547c111b993a918f68@ec2-44-210-228-110.compute-1.amazonaws.com:5432/d1dm3sv1kmqaf2"
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+#engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
 
-#engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
