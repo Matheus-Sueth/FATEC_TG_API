6852a864fd5d5a252b02c4f771402e8f7b0f5d5c
Matheus <matheuzengenharia@gmail.com>
2022-11-06 18:45:01 -0300

    refactor: ajustes no arquivo main

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ def get_credentials(credentials: HTTPBasicCredentials = Depends(security)):
             headers={'WWW-Authenticate': 'Basic'})
 
 
-@app.get("/")
+@app.get("/", tags=['Início'])
 def index():
     return {
         "Documentação Swagger": "https://selectmovietg.herokuapp.com/docs#",
@@ -67,20 +67,31 @@ def read_user_by_name_and_email(user: schemas.UserBase, db: Session = Depends(ge
           response_model=schemas.Message,
          tags=['Message'],
          dependencies=[Depends(get_credentials)])
-def create_message_for_user(user: schemas.UserBase, message: schemas.MessageCreate, db: Session = Depends(get_db)):
+def create_message_for_user(user: schemas.UserBase, message: schemas.MessageCreate, db: Session = Depends(get_db)):   
     db_user = crud.get_user_by_name(db, user)
     if db_user is None:
-        raise HTTPException(status_code=400, detail="usuario sem registro")
+        raise HTTPException(status_code=403, detail="usuario sem registro")
     return crud.create_message_user(db, message, user)
 
 
-@app.get("/messages/", 
+@app.get("/read/messages/", 
          response_model=list[schemas.Message],
          tags=['Message'],
          dependencies=[Depends(get_credentials)])
 def read_messages_by_time(hora: time, db: Session = Depends(get_db)):
     messages = crud.get_messages(db, hora)
     return messages
+
+
+@app.get("/delete/messages/", 
+         tags=['Message'],
+         dependencies=[Depends(get_credentials)])
+def delete_database(db: Session = Depends(get_db)):
+    crud.delete_message(db)
+    lista_users = crud.get_users(db=db)
+    for user in lista_users:
+        crud.delete_user(db, user.id)
+    return {"log":"banco restaurado"}
 
 
 @app.on_event("startup")
